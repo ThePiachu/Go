e5a9ba25b1a8426e0d8e297dd127af21bf884be4
ThePiachu <ThePiachu@gmail.com>
2016-02-24 23:53:52 -0800

    Created a function to replace Blobstore for Datastore.

---
+++ Datastore/FakeBlobstore.go
@@ -0,0 +1,96 @@
+package Datastore
+
+// Copyright 2016 ThePiachu. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"bytes"
+	"encoding/gob"
+	"fmt"
+
+	"github.com/ThePiachu/Go/Log"
+)
+
+const FakeBlobstoreBucket = "FakeBlobstore"
+
+type FakeBlobstoreData struct {
+	Data []byte `datastore:",noindex"`
+}
+
+const MaxDataToStore int = 1000000 //<2**20 to fit into datastore
+
+func PutInFakeBlobstore(c appengine.Context, kind, stringID string, toStore interface{}) (error) {
+	var data bytes.Buffer
+
+	enc := gob.NewEncoder(&data)
+
+	err := enc.Encode(toStore)
+	if err != nil {
+		Log.Errorf(c, "PutInFakeBlobstore error for key %s:%s - %s", kind, stringID, err)
+		return err
+	}
+
+	err = datastore.RunInTransaction(c, func(c appengine.Context) error {
+		for i:=0;;i++ {
+			f:=new(FakeBlobstoreData)
+			f.Data = data.Next(MaxDataToStore)
+			id:=makeFakeBlobstoreID(kind, stringID, i)
+			_, err:=PutInDatastoreSimple(c, FakeBlobstoreBucket, id, f)
+			if err!=nil {
+				Log.Errorf(c, "PutInFakeBlobstore - %v", err)
+				return err
+			}
+			if data.Len()==0 {
+				break
+			}
+		}
+		return nil
+    }, &datastore.TransactionOptions{XG: true})
+
+	if err!=nil {
+		Log.Errorf(c, "PutInFakeBlobstore - %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func GetFromFakeBlobstore(c appengine.Context, kind, stringID string, dst interface{}) (error) {
+	data:=bytes.NewBuffer(nil)
+	for i:=0;;i++ {
+		id:=makeFakeBlobstoreID(kind, stringID, i)
+		tmp:=new(FakeBlobstoreData)
+		err:=GetFromDatastoreSimple(c, FakeBlobstoreBucket, id, tmp)
+		if err!=nil {
+			if err==datastore.ErrNoSuchEntity {
+				break
+			}
+			Log.Errorf(c, "GetFromFakeBlobstore - %v", err)
+			return err
+		}
+		_, err = data.Write(tmp.Data)
+		if err != nil {
+			Log.Errorf(c, "GetFromFakeBlobstore - %s", err)
+			return err
+		}
+		if len(tmp.Data)<MaxDataToStore {
+			break
+		}
+	}
+
+	dec := gob.NewDecoder(data)
+	err := dec.Decode(dst)
+	if err != nil {
+		Log.Errorf(c, "GetFromFakeBlobstore - %s", err)
+		return err
+	}
+
+	return nil
+}
+
+func makeFakeBlobstoreID(kind, stringID string, i int) string {
+	return fmt.Sprintf("%s:%s:%d", kind, stringID, i)
+}
---
+++ Datastore/FakeBlobstore_test.go
@@ -0,0 +1,59 @@
+package Datastore_test
+
+// Copyright 2016 ThePiachu. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import (
+	"testing"
+	"appengine/aetest"
+	"crypto/rand"
+	"fmt"
+
+	. "github.com/ThePiachu/Go/Datastore"
+)
+
+func TestFakeBlobstore(t *testing.T) {
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+    type temp struct {
+    	ToStore string
+    	Blarg int
+    }
+    b:=new(temp)
+    b.Blarg = 100
+    data:=make([]byte, MaxDataToStore*3)
+
+	_, err = rand.Read(data)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	b.ToStore = fmt.Sprintf("%x", data)
+
+	id:="id"
+	kind:="kind"
+
+	err = PutInFakeBlobstore(c, kind, id, b)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	b2:=new(temp)
+
+	err = GetFromFakeBlobstore(c, kind, id, b2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if b2.Blarg != b.Blarg {
+		t.Errorf("Ints don't match")
+	}
+
+	if b2.ToStore != b.ToStore {
+		t.Errorf("Strings don't match")
+	}
+}
